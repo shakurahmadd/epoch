acc340094c5df88067341e03d73eeba47ddbc63a
shakurahmadd <shakurahmadd@gmail.com>
2026-04-28 11:52:17 +0100

    feat: add rainfall ingestion pipeline and generalise get_file_url

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -16,10 +16,10 @@ load_dotenv()
 CEDA_TOKEN = os.getenv("CEDA_TOKEN")
 DB_URL = os.getenv("DATABASE_URL")
 
-def get_file_url(county, src_id, station_file_name, year):
-    base_url = f'https://dap.ceda.ac.uk/badc/ukmo-midas-open/data/uk-daily-temperature-obs/dataset-version-202507/{county}/'
+def get_file_url(county, src_id, station_file_name, year, dataset):
+    base_url = f'https://dap.ceda.ac.uk/badc/ukmo-midas-open/data/{dataset}/dataset-version-202507/{county}/'
     directory_path = f'{src_id}_{station_file_name}/qc-version-1/'
-    filename = f"midas-open_uk-daily-temperature-obs_dv-202507_{county}_{src_id}_{station_file_name}_qcv-1_{year}.csv"
+    filename = f"midas-open_{dataset}_dv-202507_{county}_{src_id}_{station_file_name}_qcv-1_{year}.csv"
 
     return base_url + directory_path + filename
 
@@ -27,7 +27,7 @@ def get_file_url(county, src_id, station_file_name, year):
 def download_file(url):
     headers = {"Authorization" : f"Bearer {CEDA_TOKEN}"}
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=30)
         response.raise_for_status()
         return response
     except requests.exceptions.HTTPError as e:
@@ -53,12 +53,28 @@ def parse_observations(response):
     return observations_df_filtered
 
 
+def parse_rainfall(response):
+    observation_rain_lines = response.text.split('\n')
+    start_row = None
+    for idx, line in enumerate(observation_rain_lines):
+        if line.strip() == 'data':
+            start_row = idx
+    observations_rain_df = pd.read_csv(StringIO(response.text), skiprows=start_row + 1)
+    observations_rain_df = observations_rain_df[observations_rain_df['ob_date'] != 'end data']
+    rain_cols = ['src_id', 'ob_date', 'prcp_amt']
+    observations_rain_df = observations_rain_df[rain_cols]
+    return observations_rain_df
+
 
 def insert_observations(df, session):
     for index, row in df.iterrows():
-        db_obs_row = Observation(station_id=row['src_id'], date=row['ob_end_time'],                                 
-                                 max_air_temp = row['max_air_temp'], min_air_temp=row['min_air_temp'], prcp_amt=None)
-        session.merge(db_obs_row)
+        if 'ob_end_time' in row.index:
+            db_obs_row = Observation(station_id=row['src_id'], date=row['ob_end_time'],                                 
+                                    max_air_temp = row['max_air_temp'], min_air_temp=row['min_air_temp'], prcp_amt=None)
+            session.merge(db_obs_row)
+        else:
+            db_obs_row = Observation(station_id=row['src_id'], date=row['ob_date'], prcp_amt=row['prcp_amt'])
+            session.merge(db_obs_row)
     session.commit()
     return "Observation rows have been added"
 
@@ -72,8 +88,11 @@ def insert_stations(df, session):
     session.commit()
 
 
+
 engine = create_engine(DB_URL.replace("asyncpg", "psycopg2"))
 session_factory = sessionmaker(engine, expire_on_commit=False)
+
+
 
 def main():
     with open("midas-open_uk-daily-temperature-obs_dv-202507_station-metadata.csv", "r") as f:
@@ -86,20 +105,39 @@ def main():
     meta_data_df = meta_data_df[meta_data_df['src_id'] != 'end data']
     meta_data_df = meta_data_df[(meta_data_df['last_year'] >= 2024) & (meta_data_df['first_year'] >= 1960)]
     session = session_factory()
-    insert_stations(meta_data_df, session)
 
+    with open("midas-open_uk-daily-rain-obs_dv-202507_station-metadata.csv", "r") as f:
+        rain_meta_data = f.readlines()
+    start_row = None
+    for idx, line in enumerate(rain_meta_data):
+        if line.strip() == 'data':
+            start_row = idx
+    rain_meta_df = pd.read_csv("midas-open_uk-daily-rain-obs_dv-202507_station-metadata.csv", skiprows=start_row+1)
+    rain_meta_df = rain_meta_df[rain_meta_df['src_id'] != 'end data']
+    rain_meta_df = rain_meta_df[rain_meta_df['src_id'].isin(meta_data_df['src_id'])]
+    insert_stations(meta_data_df, session)
 
     for index, row in meta_data_df.iterrows():
         for year in range(int(row['first_year']), int(row['last_year']) + 1):
-            temp_file_url = get_file_url(row['historic_county'], row['src_id'], row['station_file_name'], year)
+            temp_file_url = get_file_url(row['historic_county'], row['src_id'], row['station_file_name'], year, "uk-daily-temperature-obs")
             response = download_file(temp_file_url)
             if response is None:
                 continue
             else:
                 temp_obvs_df = parse_observations(response)
                 insert_observations(temp_obvs_df, session)
-        print(f"Completed {row['station_name']} observation {index} our of {len(meta_data_df)}")
+        print(f"Completed {row['station_name']} temp observation {index} out of {len(meta_data_df)}")
 
+    for index, row in rain_meta_df.iterrows():
+        for year in range(int(row['first_year']), int(row['last_year']) + 1):
+            rain_file_url = get_file_url(row['historic_county'], row['src_id'], row['station_file_name'], year, "uk-daily-rain-obs")
+            response = download_file(rain_file_url)
+            if response is None:
+                continue
+            else:
+                rain_obvs_df = parse_rainfall(response)
+                insert_observations(rain_obvs_df, session)
+        print(f"Completed {row['station_name']} rain observation {index} out of {len(meta_data_df)}")
 
 
 if __name__ == "__main__": main()
