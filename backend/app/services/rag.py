1b7b4cb11e6ca8b693ab512154cd48a3a93e1f14
shakurahmadd <shakurahmadd@gmail.com>
2026-05-03 19:54:13 +0100

    feat: add Groq narrative generation to RAG service

--- backend/app/services/rag.py
+++ backend/app/services/rag.py
@@ -1,6 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from sentence_transformers import SentenceTransformer
+from groq import AsyncGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
@@ -27,3 +32,32 @@ async def get_relevant_events(db: AsyncSession, region: str, birth_year:int, k: 
                                    {"year" : birth_year, 'region' : region, "query_embedding" : query_embedding, 'k' : k, 'country' : country})
     rows = result.fetchall()
     return [dict(row._mapping) for row in rows]
+
+
+
+GROQ_API_KEY = os.getenv("GROQ_KEY")
+client = AsyncGroq(api_key=GROQ_API_KEY)
+
+async def generate_narrative(climate_data, events, region, birth_year):
+    system_prompt = """You are a climate journalist writing personalised climate summaries for UK residents.
+    Write in an conversational and friendly tone for a general audience.
+    Produce a single paragraph of 200 words.
+    Use only the climate data and historical events provided — do not add facts not present in the context.
+    Always personalise the response to the specific region and birth year given."""
+
+    user_prompt = f"Write a climate summary for someone born in {birth_year} living in {region}.\n\n Climate data: \n\n {climate_data}.                                                                                                                             Relevant historical events: {events}." 
+    chat_completion = await client.chat.completions.create(
+        messages=[{
+            "role" : "system",
+            "content" : system_prompt
+        },         
+            {
+            "role" : "user",
+            "content": user_prompt,
+        }],
+        model="llama-3.1-8b-instant",
+    )
+    return chat_completion.choices[0].message.content
+    
+
+
